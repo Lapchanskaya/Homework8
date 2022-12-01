1ad4b8134f7bd78a0ad8afcf8b33df80f8225217
Svetlana <isaeva322@mail.ru>
2022-12-01 22:30:23 +0300

    Выполнено задание 2

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,37 @@ public class Main {
         System.out.println(numbers2 [3]);
         System.out.println(numbers2 [4]);
 
+        //Задание 2
+        System.out.println( " Задание 2 ");
 
+        int [] numbers3 = new int [3];
+        numbers3 [0] = 1;
+        numbers3 [1] = 2;
+        numbers3 [2] = 3;
+        System.out.print( numbers3[0] + " , ");
+        System.out.print( numbers3[1] + " , ");
+        System.out.print(numbers3[2]+ " ; ");
+
+        System.out.println();
+
+        double [] numbers4 = { 1.57, 7.654, 9.986};
+        System.out.print( numbers4[0] + " , ");
+        System.out.print( numbers4[1] + " , ");
+        System.out.print(numbers4 [2] + " ; ");
+
+        System.out.println();
+
+        long [] numbers5 = new long [5];
+        numbers5 [0] = 7_000_000_000L ;
+        numbers5 [1] = 8_000_000_000L ;
+        numbers5 [2] = 9_000_000_000L ;
+        numbers5 [3] = 9_500_000_000L ;
+        numbers5 [4] = 10_500_000_000L ;
+        System.out.print( numbers5[0]+ " , ");
+        System.out.print( numbers5[1] + " , ");
+        System.out.print(numbers5 [2]+ " , ");
+        System.out.print(numbers5 [3]+ " , ");
+        System.out.print(numbers5 [4]+ " . ");
 
 
 
